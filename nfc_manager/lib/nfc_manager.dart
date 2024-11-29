675d229e2d39286172b13e629d32fc1373bdcf64
Ngoc Quoc Huynh <ngoc.quoci.huynh@gmail.com>
2024-11-29 12:45:24 +0100

    Add nfc_manager

---
+++ nfc_manager/lib/nfc_manager.dart
@@ -0,0 +1,7 @@
+class NfcManager {
+  factory NfcManager() => _instance;
+
+  const NfcManager._();
+
+  static const _instance = NfcManager._();
+}
---
+++ nfc_manager/test/main_test.dart
@@ -0,0 +1,6 @@
+import 'package:flutter_test/flutter_test.dart';
+
+// TODO: Add tests
+void main() {
+  test('dummy', () => expect(true, true));
+}
