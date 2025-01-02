4b9cb499535d7ffa8f2381f718be64f8e2b78677
Ngoc Quoc Huynh <ngoc.quoci.huynh@gmail.com>
2025-01-02 14:31:49 +0100

    Update example

--- nfc_manager_platform_interface/example/lib/main.dart
+++ nfc_manager_platform_interface/example/lib/main.dart
@@ -2,16 +2,60 @@ import 'package:flutter/services.dart';
 import 'package:nfc_manager_platform_interface/nfc_manager_platform_interface.dart';
 
 final class NfcManagerExamplePlatform extends NfcManagerPlatform {
-  final _methodChannel = const MethodChannel('dev.huynh/nfc_manager_example');
+  NfcManagerExamplePlatform() : super('example');
 
   static void registerWith() =>
       NfcManagerPlatform.instance = NfcManagerExamplePlatform();
 
   @override
   Future<bool> isHceSupported() async =>
-      (await _methodChannel.invokeMethod<bool>('isHceSupported'))!;
+      (await methodChannel.invokeMethod<bool>('isHceSupported'))!;
 
   @override
   Future<bool> isNfcSupported() async =>
-      (await _methodChannel.invokeMethod<bool>('isNfcSupported'))!;
+      (await methodChannel.invokeMethod<bool>('isNfcSupported'))!;
+
+  @override
+  Future<bool> isNfcEnabled() async =>
+      (await methodChannel.invokeMethod<bool>('isNfcEnabled'))!;
+
+  @override
+  Stream<String> startDiscovery({int? timeout}) => discoveryEventChannel
+      .receiveBroadcastStream({'timeout': timeout})
+      .cast<String>()
+      .handleError(
+        onStreamError,
+        test: isPlatformException,
+      );
+
+  @override
+  Future<ApduResponse> sendCommand(Command command) async {
+    try {
+      final response = await methodChannel.invokeListMethod<int>(
+        'sendCommand',
+        {'command': command.toUint8List()},
+      );
+
+      return ApduResponse.fromUint8List(Uint8List.fromList(response!));
+    } on PlatformException catch (e) {
+      throw NfcException.fromPlatformException(e);
+    }
+  }
+
+  @override
+  Stream<HostCardEmulationStatus> startEmulation({
+    required Uint8List aid,
+    required Uint8List pin,
+  }) =>
+      hostCardEmulationEventChannel
+          .receiveBroadcastStream({
+            'aid': aid,
+            'pin': pin,
+          })
+          .cast<String>()
+          .map(HostCardEmulationStatus.fromString)
+          .handleError(
+            onStreamError,
+            test: isPlatformException,
+          );
 }
