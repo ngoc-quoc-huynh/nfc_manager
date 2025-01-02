dad6c5828961986fdec89a3b69bf97eb43f92ce8
Ngoc Quoc Huynh <ngoc.quoci.huynh@gmail.com>
2025-01-02 14:31:09 +0100

    Remove Tag

--- nfc_manager_platform_interface/lib/src/tag.dart
+++
@@ -1,3 +0,0 @@
-extension type Tag(String id) {
-  String call() => id;
-}
--- nfc_manager_platform_interface/test/tag_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:nfc_manager_platform_interface/src/tag.dart';
-
-void main() {
-  test(
-    'call returns id.',
-    () => expect(Tag('id').call(), 'id'),
-  );
-}
