6c5652929a1b65b26ab8da33413a7b6c0f7ddbd2
Ngoc Quoc Huynh <ngoc.quoci.huynh@gmail.com>
2025-01-02 16:39:34 +0100

    Add feature status

--- nfc_manager_android/example/lib/main.dart
+++ nfc_manager_android/example/lib/main.dart
@@ -1,15 +1,16 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:nfc_manager_android/nfc_manager_android.dart';
 import 'package:nfc_manager_android_example/host_card_emulation.dart';
 import 'package:nfc_manager_android_example/tag_reader.dart';
 
 void main() {
-  runApp(const NfcManagerAndroidApp());
+  runApp(const NfcManagerAndroidExample());
 }
 
-class NfcManagerAndroidApp extends StatelessWidget {
-  const NfcManagerAndroidApp({super.key});
+class NfcManagerAndroidExample extends StatelessWidget {
+  const NfcManagerAndroidExample({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -26,14 +27,30 @@ class NfcManagerAndroidApp extends StatelessWidget {
         body: Center(
           child: Builder(
             builder: (context) => Column(
+              spacing: 10,
               children: [
+                _FeatureStatusView(
+                  title: 'NFC support:',
+                  // ignore: discarded_futures, future has to be unawaited.
+                  statusFuture: NfcManagerAndroidPlatform().isNfcSupported(),
+                ),
+                _FeatureStatusView(
+                  title: 'NFC enabled:',
+                  // ignore: discarded_futures, future has to be unawaited.
+                  statusFuture: NfcManagerAndroidPlatform().isNfcEnabled(),
+                ),
+                _FeatureStatusView(
+                  title: 'HCE support:',
+                  // ignore: discarded_futures, future has to be unawaited.
+                  statusFuture: NfcManagerAndroidPlatform().isHceSupported(),
+                ),
+                const Divider(),
                 FilledButton(
                   onPressed: () => unawaited(
                     Navigator.pushNamed(context, 'host-card-emulation'),
                   ),
                   child: const Text('Host Card Emulation'),
                 ),
-                const SizedBox(height: 10),
                 FilledButton(
                   onPressed: () => unawaited(
                     Navigator.pushNamed(context, 'tag-reader'),
@@ -45,6 +62,42 @@ class NfcManagerAndroidApp extends StatelessWidget {
           ),
         ),
       ),
+    );
+  }
+}
+
+class _FeatureStatusView extends StatelessWidget {
+  const _FeatureStatusView({
+    required this.title,
+    required this.statusFuture,
+  });
+
+  final String title;
+  final Future<bool> statusFuture;
+
+  @override
+  Widget build(BuildContext context) {
+    final textTheme = Theme.of(context).textTheme;
+
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      spacing: 5,
+      children: [
+        Text(
+          title,
+          style: textTheme.titleMedium,
+        ),
+        FutureBuilder<bool>(
+          future: statusFuture,
+          builder: (context, snapshot) => switch (snapshot.hasData) {
+            false => const CircularProgressIndicator(),
+            true => Text(
+                snapshot.requireData.toString(),
+                style: textTheme.bodyLarge,
+              ),
+          },
+        ),
+      ],
     );
   }
 }
